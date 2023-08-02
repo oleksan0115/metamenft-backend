aeb0dfe45a670fa52ffd1a3c7fa98960fbd84d5d
olepychev <oleksandrpychev@gmail.com>
2023-08-02 08:06:46 -0700

    complete

--- git_author.sh
+++ git_author.sh
@@ -1,6 +1,6 @@
 git filter-branch --env-filter '
-	CORRECT_NAME="bluesky0115"
-	CORRECT_EMAIL="gold.star.bee@gmail.com"
+	CORRECT_NAME="BigDreamDev"
+	CORRECT_EMAIL="asmakech115@gmail.com"
 	    export GIT_COMMITTER_NAME="$CORRECT_NAME"
 	    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
 	    export GIT_AUTHOR_NAME="$CORRECT_NAME"
